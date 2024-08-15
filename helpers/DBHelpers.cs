5fe26cb1f232a4cf450bed5b9e06a6e7bfa6936f
kimaurellano <kim.aurellano@yahoo.com>
2024-08-15 20:24:48 +0800

    Added connection string

---
+++ helpers/DBHelpers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace Jared.helpers {
+    internal class DBHelpers {
+
+        private static readonly SqliteConnection _connection = new($"Data Source=C:\\Users\\kimau\\Documents\\Visual Studio 2022\\Projects\\Jared\\jared.db");
+
+        public void InsertUser(string name) {
+            using (var connection = _connection) {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+                command.CommandText = @"INSERT INTO Patient (Name) VALUES (@Name);";
+
+                command.Parameters.AddWithValue("@Name", name);
+
+                command.ExecuteNonQuery();
+
+                MessageBox.Show("Inserted.");
+            }
+        }
+    }
+}
