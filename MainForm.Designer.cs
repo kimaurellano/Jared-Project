be31518f13bd98adccc8fd5f480c0135dcba3361
kimaurellano <kim.aurellano@yahoo.com>
2024-08-23 09:51:02 +0800

    Added camera

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -22,7 +22,8 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent() {
+        private void InitializeComponent()
+        {
             panel1 = new Panel();
             btnSearchPatient = new Button();
             btnCreateNewPatient = new Button();
@@ -31,14 +32,23 @@
             PanelPatients = new Panel();
             tabPageCapture = new TabPage();
             PanelCameraView = new Panel();
+            pictureBoxCamera = new PictureBox();
             PanelPictureList = new Panel();
             tabPageCompare = new TabPage();
             tabPagemark = new TabPage();
+            MainPanelMark = new Panel();
+            panel4 = new Panel();
+            panel3 = new Panel();
+            panel2 = new Panel();
             Settings = new TabPage();
             panel1.SuspendLayout();
             tabControlMain.SuspendLayout();
             tabPagePatients.SuspendLayout();
             tabPageCapture.SuspendLayout();
+            PanelCameraView.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxCamera).BeginInit();
+            tabPagemark.SuspendLayout();
+            MainPanelMark.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
@@ -46,16 +56,18 @@
             panel1.Controls.Add(btnSearchPatient);
             panel1.Controls.Add(btnCreateNewPatient);
             panel1.Dock = DockStyle.Left;
-            panel1.Location = new Point(3, 3);
+            panel1.Location = new Point(6, 6);
+            panel1.Margin = new Padding(6);
             panel1.Name = "panel1";
-            panel1.Size = new Size(296, 647);
+            panel1.Size = new Size(550, 1091);
             panel1.TabIndex = 0;
             // 
             // btnSearchPatient
             // 
-            btnSearchPatient.Location = new Point(3, 193);
+            btnSearchPatient.Location = new Point(6, 412);
+            btnSearchPatient.Margin = new Padding(6);
             btnSearchPatient.Name = "btnSearchPatient";
-            btnSearchPatient.Size = new Size(290, 48);
+            btnSearchPatient.Size = new Size(539, 102);
             btnSearchPatient.TabIndex = 2;
             btnSearchPatient.Text = "Search Patient";
             btnSearchPatient.UseVisualStyleBackColor = true;
@@ -63,9 +75,10 @@
             // 
             // btnCreateNewPatient
             // 
-            btnCreateNewPatient.Location = new Point(3, 139);
+            btnCreateNewPatient.Location = new Point(6, 297);
+            btnCreateNewPatient.Margin = new Padding(6);
             btnCreateNewPatient.Name = "btnCreateNewPatient";
-            btnCreateNewPatient.Size = new Size(290, 48);
+            btnCreateNewPatient.Size = new Size(539, 102);
             btnCreateNewPatient.TabIndex = 1;
             btnCreateNewPatient.Text = "Create New Patient";
             btnCreateNewPatient.UseVisualStyleBackColor = true;
@@ -80,19 +93,22 @@
             tabControlMain.Controls.Add(Settings);
             tabControlMain.Dock = DockStyle.Fill;
             tabControlMain.Location = new Point(0, 0);
+            tabControlMain.Margin = new Padding(6);
             tabControlMain.Name = "tabControlMain";
             tabControlMain.SelectedIndex = 0;
-            tabControlMain.Size = new Size(1264, 681);
+            tabControlMain.Size = new Size(1630, 1157);
             tabControlMain.TabIndex = 1;
+            tabControlMain.SelectedIndexChanged += TabControlMain_SelectedIndexChanged;
             // 
             // tabPagePatients
             // 
             tabPagePatients.Controls.Add(PanelPatients);
             tabPagePatients.Controls.Add(panel1);
-            tabPagePatients.Location = new Point(4, 24);
+            tabPagePatients.Location = new Point(8, 46);
+            tabPagePatients.Margin = new Padding(6);
             tabPagePatients.Name = "tabPagePatients";
-            tabPagePatients.Padding = new Padding(3);
-            tabPagePatients.Size = new Size(1256, 653);
+            tabPagePatients.Padding = new Padding(6);
+            tabPagePatients.Size = new Size(1614, 1103);
             tabPagePatients.TabIndex = 0;
             tabPagePatients.Text = "Patients";
             tabPagePatients.UseVisualStyleBackColor = true;
@@ -101,19 +117,21 @@
             // 
             PanelPatients.BackColor = Color.Gainsboro;
             PanelPatients.Dock = DockStyle.Fill;
-            PanelPatients.Location = new Point(299, 3);
+            PanelPatients.Location = new Point(556, 6);
+            PanelPatients.Margin = new Padding(6);
             PanelPatients.Name = "PanelPatients";
-            PanelPatients.Size = new Size(954, 647);
+            PanelPatients.Size = new Size(1052, 1091);
             PanelPatients.TabIndex = 1;
             // 
             // tabPageCapture
             // 
             tabPageCapture.Controls.Add(PanelCameraView);
             tabPageCapture.Controls.Add(PanelPictureList);
-            tabPageCapture.Location = new Point(4, 24);
+            tabPageCapture.Location = new Point(8, 46);
+            tabPageCapture.Margin = new Padding(6);
             tabPageCapture.Name = "tabPageCapture";
-            tabPageCapture.Padding = new Padding(3);
-            tabPageCapture.Size = new Size(1256, 653);
+            tabPageCapture.Padding = new Padding(6);
+            tabPageCapture.Size = new Size(1614, 1103);
             tabPageCapture.TabIndex = 1;
             tabPageCapture.Text = "Capture";
             tabPageCapture.UseVisualStyleBackColor = true;
@@ -121,54 +139,111 @@
             // PanelCameraView
             // 
             PanelCameraView.BackColor = Color.LightGray;
+            PanelCameraView.Controls.Add(pictureBoxCamera);
             PanelCameraView.Dock = DockStyle.Fill;
-            PanelCameraView.Location = new Point(258, 3);
+            PanelCameraView.Location = new Point(480, 6);
+            PanelCameraView.Margin = new Padding(6);
             PanelCameraView.Name = "PanelCameraView";
-            PanelCameraView.Size = new Size(995, 647);
+            PanelCameraView.Size = new Size(1128, 1091);
             PanelCameraView.TabIndex = 2;
+            // 
+            // pictureBoxCamera
+            // 
+            pictureBoxCamera.Location = new Point(42, 41);
+            pictureBoxCamera.Name = "pictureBoxCamera";
+            pictureBoxCamera.Size = new Size(1054, 1017);
+            pictureBoxCamera.TabIndex = 0;
+            pictureBoxCamera.TabStop = false;
             // 
             // PanelPictureList
             // 
             PanelPictureList.BackColor = Color.Gainsboro;
             PanelPictureList.Dock = DockStyle.Left;
-            PanelPictureList.Location = new Point(3, 3);
+            PanelPictureList.Location = new Point(6, 6);
+            PanelPictureList.Margin = new Padding(6);
             PanelPictureList.Name = "PanelPictureList";
-            PanelPictureList.Size = new Size(255, 647);
+            PanelPictureList.Size = new Size(474, 1091);
             PanelPictureList.TabIndex = 1;
             // 
             // tabPageCompare
             // 
-            tabPageCompare.Location = new Point(4, 24);
+            tabPageCompare.Location = new Point(8, 46);
+            tabPageCompare.Margin = new Padding(6);
             tabPageCompare.Name = "tabPageCompare";
-            tabPageCompare.Size = new Size(1256, 653);
+            tabPageCompare.Size = new Size(1614, 1103);
             tabPageCompare.TabIndex = 2;
             tabPageCompare.Text = "Compare";
             tabPageCompare.UseVisualStyleBackColor = true;
             // 
             // tabPagemark
             // 
-            tabPagemark.Location = new Point(4, 24);
+            tabPagemark.Controls.Add(MainPanelMark);
+            tabPagemark.Controls.Add(panel2);
+            tabPagemark.Location = new Point(8, 46);
+            tabPagemark.Margin = new Padding(6);
             tabPagemark.Name = "tabPagemark";
-            tabPagemark.Size = new Size(1256, 653);
+            tabPagemark.Size = new Size(1614, 1103);
             tabPagemark.TabIndex = 3;
             tabPagemark.Text = "Mark";
             tabPagemark.UseVisualStyleBackColor = true;
             // 
+            // MainPanelMark
+            // 
+            MainPanelMark.Controls.Add(panel4);
+            MainPanelMark.Controls.Add(panel3);
+            MainPanelMark.Dock = DockStyle.Fill;
+            MainPanelMark.Location = new Point(410, 0);
+            MainPanelMark.Margin = new Padding(6);
+            MainPanelMark.Name = "MainPanelMark";
+            MainPanelMark.Size = new Size(1204, 1103);
+            MainPanelMark.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Gainsboro;
+            panel4.Dock = DockStyle.Fill;
+            panel4.Location = new Point(0, 0);
+            panel4.Margin = new Padding(6);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(1204, 890);
+            panel4.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.WhiteSmoke;
+            panel3.Dock = DockStyle.Bottom;
+            panel3.Location = new Point(0, 890);
+            panel3.Margin = new Padding(6);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1204, 213);
+            panel3.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 0);
+            panel2.Margin = new Padding(6);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(410, 1103);
+            panel2.TabIndex = 0;
+            // 
             // Settings
             // 
-            Settings.Location = new Point(4, 24);
+            Settings.Location = new Point(8, 46);
+            Settings.Margin = new Padding(6);
             Settings.Name = "Settings";
-            Settings.Size = new Size(1256, 653);
+            Settings.Size = new Size(1614, 1103);
             Settings.TabIndex = 4;
             Settings.Text = "Settings";
             Settings.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1264, 681);
+            ClientSize = new Size(1630, 1157);
             Controls.Add(tabControlMain);
+            Margin = new Padding(6);
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Jared";
@@ -177,6 +252,10 @@
             tabControlMain.ResumeLayout(false);
             tabPagePatients.ResumeLayout(false);
             tabPageCapture.ResumeLayout(false);
+            PanelCameraView.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBoxCamera).EndInit();
+            tabPagemark.ResumeLayout(false);
+            MainPanelMark.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -193,6 +272,11 @@
         private TabPage tabPagemark;
         private TabPage Settings;
         private Panel PanelPictureList;
+        private Panel panel2;
+        private Panel MainPanelMark;
+        private Panel panel4;
+        private Panel panel3;
         private Panel PanelCameraView;
+        private PictureBox pictureBoxCamera;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -1,9 +1,14 @@
+using AForge.Video.DirectShow;
+using AForge.Video;
 using Microsoft.Data.Sqlite;
 using System.Data;
 using System.Windows.Forms;
 
 namespace Jared {
     public partial class MainForm : Form {
+
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
 
         public MainForm() {
             InitializeComponent();
@@ -22,6 +27,47 @@ namespace Jared {
             patientsPanel.Controls.Clear();
             userControl.Dock = DockStyle.Fill;
             patientsPanel.Controls.Add(userControl);
+        }
+
+        private void InitializeCamera() {
+            // Get the available video devices
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            if (videoDevices.Count == 0) {
+                MessageBox.Show("No video sources found");
+                return;
+            }
+
+            // Choose the first available video device
+            videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+
+            // Set NewFrame event handler to capture frames from the video source
+            videoSource.NewFrame += new NewFrameEventHandler(videoSource_NewFrame);
+
+            // Start the video source
+            videoSource.Start();
+        }
+
+        private void videoSource_NewFrame(object sender, NewFrameEventArgs eventArgs) {
+            // Get the frame from the camera
+            Bitmap frame = (Bitmap)eventArgs.Frame.Clone();
+
+            // Display the frame in the PictureBox
+            pictureBoxCamera.Image = frame;
+        }
+
+        private void TabControlMain_SelectedIndexChanged(object sender, EventArgs e) {
+            if (tabControlMain != null) {
+                if (tabControlMain.SelectedIndex == 1) {
+                    InitializeCamera();
+                } else {
+                    // Stop the video source when closing the form
+                    if (videoSource != null && videoSource.IsRunning) {
+                        videoSource.SignalToStop();
+                        videoSource.WaitForStop();
+                    }
+                }
+            } 
         }
     }
 }
