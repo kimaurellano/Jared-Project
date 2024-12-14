83284814eff9e94bd7d9956c9237dcfcfd127d00
kimaurellano <kim.aurellano@yahoo.com>
2024-12-14 15:26:59 +0800

    Update - added profile picturebox in selectedpatient

--- UserControls/SelectedPersonUserControl.Designer.cs
+++ UserControls/SelectedPersonUserControl.Designer.cs
@@ -23,20 +23,23 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SelectedPersonUserControl));
             LabelSelectedPatient = new Label();
             GroupBoxSelectedPatient = new GroupBox();
-            LabelGender = new Label();
-            LabelIDCard = new Label();
-            LabelPhoneNumber = new Label();
-            LabelAddress = new Label();
             LabelRemarks = new Label();
+            LabelAddress = new Label();
+            LabelPhoneNumber = new Label();
+            LabelIDCard = new Label();
+            LabelGender = new Label();
+            PictureBoxProfile = new PictureBox();
             GroupBoxSelectedPatient.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)PictureBoxProfile).BeginInit();
             SuspendLayout();
             // 
             // LabelSelectedPatient
             // 
             LabelSelectedPatient.AutoSize = true;
-            LabelSelectedPatient.Location = new Point(70, 102);
+            LabelSelectedPatient.Location = new Point(62, 472);
             LabelSelectedPatient.Margin = new Padding(6, 0, 6, 0);
             LabelSelectedPatient.Name = "LabelSelectedPatient";
             LabelSelectedPatient.Size = new Size(158, 32);
@@ -45,6 +48,7 @@
             // 
             // GroupBoxSelectedPatient
             // 
+            GroupBoxSelectedPatient.Controls.Add(PictureBoxProfile);
             GroupBoxSelectedPatient.Controls.Add(LabelRemarks);
             GroupBoxSelectedPatient.Controls.Add(LabelAddress);
             GroupBoxSelectedPatient.Controls.Add(LabelPhoneNumber);
@@ -60,55 +64,68 @@
             GroupBoxSelectedPatient.TabStop = false;
             GroupBoxSelectedPatient.Text = "Current Patient";
             // 
-            // LabelGender
+            // LabelRemarks
             // 
-            LabelGender.AutoSize = true;
-            LabelGender.Location = new Point(71, 195);
-            LabelGender.Margin = new Padding(6, 0, 6, 0);
-            LabelGender.Name = "LabelGender";
-            LabelGender.Size = new Size(92, 32);
-            LabelGender.TabIndex = 1;
-            LabelGender.Text = "Gender";
-            // 
-            // LabelIDCard
-            // 
-            LabelIDCard.AutoSize = true;
-            LabelIDCard.Location = new Point(70, 283);
-            LabelIDCard.Margin = new Padding(6, 0, 6, 0);
-            LabelIDCard.Name = "LabelIDCard";
-            LabelIDCard.Size = new Size(93, 32);
-            LabelIDCard.TabIndex = 2;
-            LabelIDCard.Text = "ID Card";
-            // 
-            // LabelPhoneNumber
-            // 
-            LabelPhoneNumber.AutoSize = true;
-            LabelPhoneNumber.Location = new Point(70, 369);
-            LabelPhoneNumber.Margin = new Padding(6, 0, 6, 0);
-            LabelPhoneNumber.Name = "LabelPhoneNumber";
-            LabelPhoneNumber.Size = new Size(170, 32);
-            LabelPhoneNumber.TabIndex = 3;
-            LabelPhoneNumber.Text = "PhoneNumber";
+            LabelRemarks.AutoSize = true;
+            LabelRemarks.Location = new Point(63, 922);
+            LabelRemarks.Margin = new Padding(6, 0, 6, 0);
+            LabelRemarks.Name = "LabelRemarks";
+            LabelRemarks.Size = new Size(103, 32);
+            LabelRemarks.TabIndex = 5;
+            LabelRemarks.Text = "Remarks";
             // 
             // LabelAddress
             // 
             LabelAddress.AutoSize = true;
-            LabelAddress.Location = new Point(71, 460);
+            LabelAddress.Location = new Point(63, 830);
             LabelAddress.Margin = new Padding(6, 0, 6, 0);
             LabelAddress.Name = "LabelAddress";
             LabelAddress.Size = new Size(98, 32);
             LabelAddress.TabIndex = 4;
             LabelAddress.Text = "Address";
             // 
-            // LabelRemarks
+            // LabelPhoneNumber
             // 
-            LabelRemarks.AutoSize = true;
-            LabelRemarks.Location = new Point(71, 552);
-            LabelRemarks.Margin = new Padding(6, 0, 6, 0);
-            LabelRemarks.Name = "LabelRemarks";
-            LabelRemarks.Size = new Size(103, 32);
-            LabelRemarks.TabIndex = 5;
-            LabelRemarks.Text = "Remarks";
+            LabelPhoneNumber.AutoSize = true;
+            LabelPhoneNumber.Location = new Point(62, 739);
+            LabelPhoneNumber.Margin = new Padding(6, 0, 6, 0);
+            LabelPhoneNumber.Name = "LabelPhoneNumber";
+            LabelPhoneNumber.Size = new Size(170, 32);
+            LabelPhoneNumber.TabIndex = 3;
+            LabelPhoneNumber.Text = "PhoneNumber";
+            // 
+            // LabelIDCard
+            // 
+            LabelIDCard.AutoSize = true;
+            LabelIDCard.Location = new Point(62, 653);
+            LabelIDCard.Margin = new Padding(6, 0, 6, 0);
+            LabelIDCard.Name = "LabelIDCard";
+            LabelIDCard.Size = new Size(93, 32);
+            LabelIDCard.TabIndex = 2;
+            LabelIDCard.Text = "ID Card";
+            // 
+            // LabelGender
+            // 
+            LabelGender.AutoSize = true;
+            LabelGender.Location = new Point(63, 565);
+            LabelGender.Margin = new Padding(6, 0, 6, 0);
+            LabelGender.Name = "LabelGender";
+            LabelGender.Size = new Size(92, 32);
+            LabelGender.TabIndex = 1;
+            LabelGender.Text = "Gender";
+            // 
+            // PictureBoxProfile
+            // 
+            PictureBoxProfile.BackColor = Color.Gainsboro;
+            PictureBoxProfile.Image = (Image)resources.GetObject("PictureBoxProfile.Image");
+            PictureBoxProfile.InitialImage = (Image)resources.GetObject("PictureBoxProfile.InitialImage");
+            PictureBoxProfile.Location = new Point(62, 86);
+            PictureBoxProfile.Name = "PictureBoxProfile";
+            PictureBoxProfile.Padding = new Padding(50);
+            PictureBoxProfile.Size = new Size(296, 320);
+            PictureBoxProfile.SizeMode = PictureBoxSizeMode.Zoom;
+            PictureBoxProfile.TabIndex = 13;
+            PictureBoxProfile.TabStop = false;
             // 
             // SelectedPersonUserControl
             // 
@@ -123,6 +140,7 @@
             Paint += SelectedPersonUserControl_Paint;
             GroupBoxSelectedPatient.ResumeLayout(false);
             GroupBoxSelectedPatient.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)PictureBoxProfile).EndInit();
             ResumeLayout(false);
         }
 
@@ -135,5 +153,6 @@
         private Label LabelRemarks;
         private Label LabelAddress;
         private Label LabelPhoneNumber;
+        private PictureBox PictureBoxProfile;
     }
 }
